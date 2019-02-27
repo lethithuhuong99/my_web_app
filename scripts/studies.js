d3f3f3a77eee32e31f79169d860fe02dc490da90
lethithuhuong99 <huong.le170205@vnuk.edu.vn>
2019-02-27 10:32:46 +0700

    27/2/19

---
+++ scripts/studies.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+  //alert ("Hello!");
+  var tds = document.getElementsByTagName('td');
+  var highlighteds = document.getElementsByClassName('highlighted');
+  var myTd = document.getElementById('my-td');
+}
