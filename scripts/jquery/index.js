85ed3965f73445023fb853785326d3eb03d057b7
lethithuhuong99 <huong.le170205@vnuk.edu.vn>
2019-03-08 18:11:56 +0700

    8/3/19

--- scripts/jquery/index.js
+++ scripts/jquery/index.js
@@ -1,6 +1,31 @@
-// $(window).resize(logScreenSize);
+$(window).resize(logScreenSize);
 
 $(function(){
-  // logScreenSize();
+  logScreenSize();
   resizeElements(['.my-button-people-name', '.my-button-jump-to-people'])
+
+  if(!Modernizr.mq('(min-width: 576px)')) {
+    $('.my-button-people-name').css('width', '180px');
+  }
+
+  $('.my-button-people-name').on('click', function(){
+    // $(this).removeClass("btn-secondary").addClass("btn-danger");
+    $(this).toggleClass("btn-secondary btn-danger");
+    var $button = $(this).closest("div").find(".my-button-jump-to-people");
+    $button.toggleClass("d-none");
+
+    if(Modernizr.mq('(min-width: 576px)')) {
+      resizeElements(['.my-button-people-name', '.my-button-jump-to-people']);
+    }
+
+    else {
+      resizeElements(['.my-button-jump-to-people']);
+      $('.my-button-people-name').css('width', '180px');
+    }
+
+
+    var id = $button.find("a").attr("href");
+    $(id).toggleClass("d-none");
+
+  });
 });
